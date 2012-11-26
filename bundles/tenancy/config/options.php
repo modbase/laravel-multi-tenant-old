b8cfe513c894162fea3310f28082c852b2cd6e34
Stijn Geselle <stijn.geselle@gmail.com>
2012-11-26 17:24:26 +0100

    Fixed comma typo and added new option

--- bundles/tenancy/config/options.php
+++ bundles/tenancy/config/options.php
@@ -4,9 +4,10 @@
  * ============================ */
 
 return array(
+    'enable_cpanel' => false,           // Enable/disable cPanel features
 	'cpanel_user' => 'your_cpanel',		// Your cPanel username
 	'cpanel_pass' => 'your_pass',		// Your cPanel password
-	'cpanel_host' => 'your_hostname'	// The hostname (or IP) where cPanel is located
+	'cpanel_host' => 'your_hostname',	// The hostname (or IP) where cPanel is located
 	'cpanel_port' => 2083,				// This port should be ok by default
 	'debug' => false,					// Display debug messages of the Cpanel class
 );
